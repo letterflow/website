ce52688aa685d186718899af50da403ccaaa66d4
Nelson Dominguez <nelsilicous@gmail.com>
2022-10-05 10:10:53 +0200

    style(app): :art: run nx format

--- apps/letterflow/src/app/app.module.ts
+++ apps/letterflow/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
