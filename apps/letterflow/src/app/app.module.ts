ecc7011d7a54269719d33563a48f155265c74fbb
Nelson Dominguez <nelsilicous@gmail.com>
2022-10-05 12:42:50 +0200

    feat(ui): :lipstick: add core app ui layout

--- apps/letterflow/src/app/app.module.ts
+++ apps/letterflow/src/app/app.module.ts
@@ -1,13 +1,28 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
 import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {AppComponent} from './app.component';
+
+export const MATERIAL_COMPONENTS = [
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, ...MATERIAL_COMPONENTS],
   providers: [],
   bootstrap: [AppComponent],
 })
