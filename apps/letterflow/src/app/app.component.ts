9d9a3773d428a821f226222938d1218f3cdbb367
Nelson Dominguez <nelsilicous@gmail.com>
2022-10-05 10:08:45 +0200

    refactor(app): :fire: remove nx-generated boilerplate

--- apps/letterflow/src/app/app.component.ts
+++ apps/letterflow/src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@website/api-interfaces';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'website-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
-}
+export class AppComponent {}
--- apps/letterflow/src/app/app.module.ts
+++ apps/letterflow/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
